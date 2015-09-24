68aadc44652944564301edf0dfa83926644c97c6
Brian Friesen <brianfriesen@quickenloans.com>
2015-09-24 18:51:20 -0400

    string

--- JsonParser.Tests/JsonParseTests.cs
+++ JsonParser.Tests/JsonParseTests.cs
@@ -53,5 +53,15 @@ namespace JsonParser.Tests
 
             Assert.That(result, Is.EqualTo(@"""Ow."" -my pancreas"));
         }
+
+        [Test, Ignore]
+        public void StringWithEscapedBackslashReturnsString()
+        {
+            var json = @"""c:\\temp\\file.txt""";
+
+            var result = Json.Parse(json);
+
+            Assert.That(result, Is.EqualTo(@"c:\temp\file.txt"));
+        }
     }
 }
