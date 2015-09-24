13dd06950931f8e2e15d7242084bba34e1c50481
Brian Friesen <brianfriesen@quickenloans.com>
2015-09-24 18:51:56 -0400

    string

--- JsonParser/Parser.cs
+++ JsonParser/Parser.cs
@@ -22,9 +22,16 @@ namespace JsonParser
                 from quote in Parse.Char('"')
                 select quote;
 
+            var escapedBackslashParser =
+                from firstBackslash in Parse.Char('\\')
+                from secondBackslash in Parse.Char('\\')
+                select secondBackslash;
+
             return
                 from openQuote in Parse.Char('"')
-                from value in escapedQuoteParser.Or(Parse.CharExcept('"')).Many().Text()
+                from value in escapedBackslashParser
+                    .Or(escapedQuoteParser)
+                    .Or(Parse.CharExcept('"')).Many().Text()
                 from closeQuote in Parse.Char('"')
                 select value;
         }
